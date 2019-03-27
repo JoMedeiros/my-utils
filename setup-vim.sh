11df45300d0e8bfc9d91e30911e37f8ac07cf9c0
JoMedeiros <josivan.medeiros@live.com>
2019-03-26 23:37:25 -0300

    wget plug.vim

--- setup-vim.sh
+++ setup-vim.sh
@@ -3,7 +3,7 @@
 # Download contents
 wget -P ~/ https://raw.githubusercontent.com/JoMedeiros/my-utils/master/.vimrc
 mkdir -p ~/.vim/autoload 
-wget -P ~/.vim/autoload https://github.com/junegunn/vim-plug/blob/master/plug.vim
+wget -P ~/.vim/autoload https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Run the scripts
 vim +'PlugInstall --sync' +qa
 ~/.vim/plugged/YouCompleteMe/install.sh
